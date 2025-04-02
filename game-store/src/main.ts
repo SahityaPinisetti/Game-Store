06517c5d032b67fd75ae4d8543d20180c89b62ee
LAKSHMI SATYA SRI YARAMATI <155799883+SatyaSri2@users.noreply.github.com>
2025-04-02 16:57:48 +0530

    Add files via upload

--- game-store/src/main.ts
+++ game-store/src/main.ts
@@ -1,6 +1,24 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component';
+// import { bootstrapApplication } from '@angular/platform-browser';
+// import { AppComponent } from './app/app.component';
+// import { provideRouter } from '@angular/router';
+// import { HomeComponent } from './app/pages/home/home.component';
+// import { CartComponent } from './app/pages/cart/cart.component';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+// bootstrapApplication(AppComponent, {
+//   providers: [
+//     provideRouter([
+//       { path: '', component: HomeComponent }, // ✅ Home page route
+//       { path: 'cart', component: CartComponent } // ✅ Cart page route
+//     ])
+//   ]
+// });
+
+import { bootstrapApplication } from '@angular/platform-browser';
+import { AppComponent } from './app/app.component';
+import { provideRouter } from '@angular/router';
+import { routes } from './app/app.routes';
+import { provideHttpClient } from '@angular/common/http'; 
+
+bootstrapApplication(AppComponent, {
+  providers: [provideRouter(routes), provideHttpClient()]
+}).catch(err => console.error(err));
